ebaf7d425943e4456a96df78fad7a85f9541fd23
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-04-01 01:36:57 -0300

    DB: Added new structure to Cliente table

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,5 @@
 /* creating tables */
-CREATE TABLE Cliente (cliente_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT);
+create table Cliente(cliente_id int not null auto_increment primary key, nome text, endereco text, cc text, data text, cpf text, cnh text, necessidades_especiais tinyint(1) default null);
+
+/* inserting an example value */
+insert into Cliente (nome, endereco, cc, data, cpf, cnh, necessidades_especiais) values ("João Ninguém da Silva", "Rua dos Bobos, 0", "1111 2222 3333 4444", "30/03/1999", "123.456.789-00", "000 111 222 33", 0);
