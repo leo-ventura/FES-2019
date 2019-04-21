12a16cdfe648a1142a6ccc2267f942ea8088e81c
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-04-21 00:19:48 -0300

    [db] Updating database structure

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,64 @@
 /* creating tables */
-create table Cliente(clienteId int not null auto_increment primary key, nome text, endereco text, cc text, data text, cpf text, cnh text, necessidadesEspeciais tinyint(1) default null, dataDeCriacao text);
+CREATE TABLE Cliente (
+    ClienteId int not null auto_increment primary key,
+    Nome varchar(255),
+    Endereco varchar(255),
+    CreditCard varchar(16),
+    DataDeNascimento varchar(10),
+    CPF varchar(11),
+    CNH varchar(11),
+    NecessidadesEspeciais tinyint(1) default null,
+    DataDeCadastro varchar(10),
+    DataDeAlteracao varchar(10)
+) ENGINE=INNODB;
+
+CREATE TABLE Veiculos (
+    foreign key (CPF) references Cliente (CPF),
+    Modelo varchar(40),
+    Marca varchar(40),
+    Placa varchar(10),
+    Grupo varchar(5),
+    DataInicio varchar(10),
+    DataTermino varchar(10)
+) ENGINE=INNODB;
 
 /* inserting an example value */
-insert into Cliente (nome, endereco, cc, data, cpf, cnh, necessidades_especiais) values ("João Ninguém da Silva", "Rua dos Bobos, 0", "1111 2222 3333 4444", "30-03-1999", "123.456.789-00", "000 111 222 33", 0, "13-04-2019");
+INSERT INTO Cliente (
+    nome,
+    endereco,
+    cc,
+    dataDeNascimento,
+    cpf,
+    cnh,
+    necessidadesEspeciais,
+    dataDeCadastro,
+    dataDeAlteracao
+) VALUES (
+    "João Ninguém da Silva",
+    "Rua dos Bobos, 0",
+    "1111 2222 3333 4444",
+    "30/03/1999",
+    "12345678900",
+    "000 111 222 33",
+    0,
+    "20/04/2019",
+    "20/04/2019"
+);
+
+INSERT INTO Veiculos (
+    CPF,
+    Modelo,
+    Marca,
+    Placa,
+    Grupo,
+    DataInicio,
+    DataTermino
+) VALUES (
+    "12345678900",
+    "Model X",
+    "Tesla",
+    "P4D4R14",
+    "sp",
+    "20/04/2019",
+    "23/04/2019"
+)
